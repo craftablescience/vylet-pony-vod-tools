501f0e25bb284e5a71d1d11238767feca861bb6f
craftablescience <me@craftablescience.info>
2025-12-17 21:44:37 -0800

    fix: always use correct chat-downloader package

--- _setup.sh
+++ _setup.sh
@@ -3,8 +3,7 @@ python -m venv .venv
 source .venv/bin/activate
 
 pip install --upgrade pip
-pip install pillow setuptools webvtt-py yt-dlp
-pip install ext/chat-downloader
+pip install chat-downloader@git+https://github.com/Indigo128/chat-downloader pillow setuptools webvtt-py yt-dlp
 
 cmake ext/subchat -B ext/subchat/build -DBUILD_GUI=OFF
 cmake --build ext/subchat/build
